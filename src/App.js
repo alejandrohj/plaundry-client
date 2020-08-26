b285cdef9270f9f686108c22bf0ca8860af9e54e
Lanette Lanting <lanettelanting@gmail.com>
2020-08-26 15:00:48 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Switch, Route, withRouter} from 'react-router-dom'
+import {API_URL} from './config'
+import axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
