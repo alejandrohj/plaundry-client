177147084d1bc9f6ca96587021526d770c5b00c7
Lanette Lanting <lanettelanting@gmail.com>
2020-09-02 11:04:11 +0200

    small changes

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -14,13 +14,14 @@ export default function OrderCard(props) {
                 status==='to pick up'? <p> Date: <b>{pickUp.slice(0,10)}</b></p> :
                 <p> Date: <b>{delivery.slice(0,10)}</b></p>
               }
-              <h5>{userId.name.firstName}</h5>
-              <h6>{userId.address.city}</h6>
-              <p>{status}</p>
               {
                 status==='to pick up'? <p> Pick up at : <b>{pickUp.slice(11,16)}</b></p> :
                 <p> Deliver at: <b>{delivery.slice(11,16)}</b></p>
               }
+              <h5>{userId.name.firstName}</h5>
+              <h6>{userId.address.city}</h6>
+              <p>{status}</p>
+              
             </div>
             <div>
               <Link  style ={{marginLeft:'30px'}} to={`/admin/delivery/${_id}/details`}><Button className="general-btn">Details</Button></Link>
